786e1d68b05e457675bcb5aa96e0e3c47d8ddc04
tushar-26 <savdiyatushar17@gmail.com>
2024-09-15 14:09:53 +0530

    add

--- multi-array.php
+++ multi-array.php
@@ -1,8 +1,59 @@
+<?php 
+
+//^multidimentional array 1,2,3,4,5 etc.....verticals and horizontals
+
+$list = array(
+             array("tushar","savdiya","reader"),
+             array("digvijay","parmar","hardworker"),
+             array("rohit","raval","biker"),
+             array("vrajesh","akheniya","mutthal"),
+);
+
+print_r($list);
+echo "<br>";
+var_dump($list);
+echo "<br>";
+echo "<br>";
+
+for($i = 0; $i<4; $i++){
+
+    for($x = 0; $x<3; $x++){
+      echo $list [$i][$x];
+      echo "<br>";
+    }
+}
+foreach($list as $list1){
+    echo "<br>";
+    print_r($list1);
+}
+
+echo "<br>";
+echo "<br>";
+
+for($z = 0; $z<4; $z++){
+    foreach($list[$z] as $data){
+        echo $data;
+        echo "<br>";
+    }
+}
+echo "<br>";
+echo "<br>";
+
+print_r($list[0]);     //to view array
+echo "<br>";
+echo $list[0][2];       //to view perticular value in array
+
+
+echo "<pre>";    // to view any array in reliable formate
+foreach($list as $li){
+    print_r($li[0]);
+}
+
+print_r($list);
+?>
+
+
 <?php
-
-//^multidimentional array 1,2,3,4,5 etc.....  multi verticals and horizontals
-
-
 $student = array(
                  array("tushar","php",60),         //0
                  array("digvijay","C++",34),       //1  
